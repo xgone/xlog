d433f1dbfbefbd39b0632f51879e864dc030d530
Gone <xiagone@gmail.com>
2014-12-02 17:07:35 +0800

    modify config

--- manage/config/cimongo.php
+++ manage/config/cimongo.php
@@ -17,7 +17,7 @@ $config['pass'] = "gone123";
 //$config['query_safety'] = TRUE;
 $config['w'] = 0;
 //If running in auth mode and the user does not have global read/write then set this to true
-//$config['db_flag'] = TRUE;
+$config['db_flag'] = TRUE;
 
 //consider these config only if you want to store the session into mongoDB
 //They will be used in MY_Session.php
