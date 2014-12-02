8a5ff8a3eacff12a9d1048f43a45b1938439a06c
Gone <xiagone@gmail.com>
2014-12-02 12:42:23 +0800

    add mongo lib

--- manage/controllers/home.php
+++ manage/controllers/home.php
@@ -4,6 +4,12 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->library("cimongo/cimongo");
+		//insert
+		$this->cimongo->insert('test', array('id' => 1, 'str' => 'hello mongo'));
+		//get
+		$query = $this->cimongo->limit(1)->get_where('test', array('id' => 1));
+		print_r($query);
 		$this->load->view('home');
 	}
 }
